d4e0666fec075c60d4db0754d732db490f6d9c69
THISISAGOODNAME <yangyj19931231@gmail.com>
2025-03-28 18:54:51 +0800

    Enable imgui docking

--- main.cpp
+++ main.cpp
@@ -27,9 +27,13 @@ void init() {
     sg_setup(&_sg_desc);
 
     simgui_desc_t _simgui_desc{};
+    _simgui_desc.logger.func = slog_func;
     simgui_setup(&_simgui_desc);
+    ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
  }
 
+static bool show_test_window = true;
+static bool show_another_window = false;
 void frame() {
     const double dt = sapp_frame_duration();
 
@@ -39,9 +43,29 @@ void frame() {
 
     // imgui
     {
-        ImGuiIO& io = ImGui::GetIO();
+        // 1. Show a simple window
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+        static float f = 0.0f;
+        ImGui::Text("Drag windows over one another!");
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+        ImGui::ColorEdit3("clear color", &state.graphics.pass_action.colors[0].clear_value.r);
+        if (ImGui::Button("Test Window")) show_test_window ^= 1;
+        if (ImGui::Button("Another Window")) show_another_window ^= 1;
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
-        ImGui::ShowDemoWindow();
+        // 2. Show another simple window, this time using an explicit Begin/End pair
+        if (show_another_window) {
+            ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiCond_FirstUseEver);
+            ImGui::Begin("Another Window", &show_another_window);
+            ImGui::Text("Hello");
+            ImGui::End();
+        }
+
+        // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowDemoWindow()
+        if (show_test_window) {
+            ImGui::SetNextWindowPos(ImVec2(460, 20), ImGuiCond_FirstUseEver);
+            ImGui::ShowDemoWindow();
+        }
     }
 
     // graphics pass
